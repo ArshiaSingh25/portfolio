d8141a986df2eda421fb976cc28f9116d41677f9
Arshia <arshiasingh9787@gmail.com>
2025-04-12 17:05:23 +0530

    navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "@fontsource/jetbrains-mono";
-
+import Navbar from "./navbar";
 
 
 export default function App() {
   return (
     <div className="bg-purple-300 h-screen"> 
+     <Navbar/>
+    <h1 className=" font-mono font-bold text-3xl px-32 pt-20 rounded-lg">hi,i'm arshia! </h1>
    
-    <h1 className=" font-mono font-bold text-2xl mx-20 pt-20 rounded-lg">hi,i am arshia </h1>
-    </div>
+   </div>
   )
 }
---
+++ src/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Projects from './projects';
+import Blogs from './blogs';
+import Contact from './contact';
+
+const Navbar = () => {
+  return (
+    <div className=' absolute top-8 right-14 flex space-x-6 text-2xl font-mono'>
+     <Projects/>
+     <Blogs/>
+     <Contact/>
+    </div>
+  )
+}
+
+export default Navbar;
