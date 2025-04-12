2f717436b5e50a5c10b0ba4599bd5c55468c30e5
Arshia Singh <arshiasingh@Arshias-MacBook-Pro.local>
2025-04-12 15:14:01 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "@fontsource/jetbrains-mono";
+
+
+
+export default function App() {
+  return (
+    <div className="bg-purple-300 h-screen"> 
+   
+    <h1 className=" font-mono font-bold text-2xl mx-20 pt-20 rounded-lg">hi,i am arshia </h1>
+    
+   
+    </div>
+  )
+}
