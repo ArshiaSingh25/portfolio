3b755b19bb91b3e60dae5b48fa75eca9ec85ba0e
Arshia <arshiasingh9787@gmail.com>
2025-04-12 15:25:30 +0530

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ export default function App() {
     <div className="bg-purple-300 h-screen"> 
    
     <h1 className=" font-mono font-bold text-2xl mx-20 pt-20 rounded-lg">hi,i am arshia </h1>
-    
-   
     </div>
   )
 }
